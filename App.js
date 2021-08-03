e592dd1af530816ca847782b4606fc89aa6810f7
greyspurv2 <35030409+greyspurv2@users.noreply.github.com>
2021-08-03 10:59:37 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,81 +1,35 @@
-import React from 'react';
-import '../App.css';
-import DefiCards from './components/Deficards';
-import Body from './components/Body';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Navbar from './components/Navbar';
-import { Container, Row, Col } from 'reactstrap';
-import { render } from '@testing-library/react';
+import React, { Fragment } from "react";
+import Link from 'next/link';
 
-class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            people: [{
-                    project: "Honeyswap",
-                    title: "hello",
-                    description: "This is the newest craze in Defi"
-                },
-                {
-                    project: "Honeyswap",
-                    title: "This is a new Defi project",
-                    description: "This is the newest craze in Defi"
-                },
-                {
-                    project: "Honeyswap",
-                    title: "This is a new Defi project",
-                    description: "This is the newest craze in Defi"
-                }
-            ]
-        }
-    }
-}
+// import static data
+import { projects } from "../../cards/constants";
 
-
-render()
-let peopleCards = this.state.people.map(person => {
-        return ( <
-            Col sm = "" >
-            <
-            DefiCards project = { DefiCards }
-            /> < /
-            Col >
-        )
-
-    }
-
-)
-
-
-
-function DefiCardsRow() {
-    return ( <
-        div className = "DefiCards" >
-        <
-        header >
-        <
-        body >
-        <
-        Container fluid >
-        <
-        Row > { DefiCards } <
-        /Row> < /
-        Container >
-
-        <
-        /body> <
-        footer className = "Footer" >
-
-        <
-        /footer>
-
-        <
-        /header> < /
-        div >
+const card = () => {
+    return ( 
+        <Fragment className="main" >
+        
+        <div className = "cards"> {
+            projects.map((item, index) => {
+                return ( 
+                    <Link href={`${item.URL}`}>
+                        <a>
+                    <div className = "card"></div>
+                    key = { index }
+                    
+                    <img src = { `../../static/assets/images/${item.Image}` }
+                    className = "img-fluid"
+                    width = "100" />
+                    
+                    
+                    <p > { item.project } </p> 
+                    <p > { item.title } </p> <p > { item.description } </p> </div>
+                    </a>
+                    </Link>
+                );
+            })
+        } </div>
+         </Fragment>
     );
-}
+};
 
-
-
-export default App;
+export default card;
